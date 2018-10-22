65df024e2e76ec624af019136922231d92ea552c
Pierre BELLON <bellon.pierre@gmail.com>
2018-10-22 14:52:07 +0200

    Add configure.sh

---
+++ configure.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+
+bashrc(){
+    if [ -f ~/.bashrc ]; then
+        backup_name=~/.bashrc.bak
+        if [ -f $backup_name ]; then
+            n=$(ls -Al ~ | grep $backup_name | wc -l)
+            backup_name="$backup_name.$n"
+        fi
+        cp ~/.bashrc $backup_name
+    fi
+    cp ./.bashrc ~/.bashrc
+}
+
+opt(){
+    if [ ! -d ~/opt ]; then
+        mkdir ~/opt/
+    fi;
+    cp ./opt ~/opt -r 
+}
+
+install(){
+    bashrc
+    opt
+}
+
+
+echo "Installing..."
+install
+echo "...Done"
